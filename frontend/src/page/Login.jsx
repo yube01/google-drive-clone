8e91284d586d7d0b58e874e759f7ffadcc261107
yube01 <yubraja008@gmail.com>
2023-07-25 15:06:44 +0545

    login and regitser style complete

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react"
 import "./authStyle.scss"
+import { Link } from "react-router-dom"
 
 
 
 const Login = () => {
+
+
+  const [email,setEmail]  = useState("")
+  const[password,setPassword] = useState("")
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+  }
+
   return (
-    <div>
-      Login
+    <div className="login">
+
+    <div className="container">
+      <p className="title">Login</p>
+      <form onSubmit={handleSubmit}>
+        <input type="email"  placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        <input type="submit" value="Login" className="btn"/>
+      </form>
+      <p>Does't have a account?? <Link to="/signup" style={{textDecoration:"none",color:"inherit"}}>
+          Signup
+        </Link> </p>
     </div>
+      
+    
+  </div>
   )
 }
 
--- frontend/src/page/Signup.jsx
+++ frontend/src/page/Signup.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import "./authStyle.scss"
+import {Link} from 'react-router-dom'
+
+
 
 const Signup = () => {
+
+
+  const [email,setEmail]  = useState("")
+  const[password,setPassword] = useState("")
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+  }
+
   return (
-    <div>
-        Signup
+    <div className="register">
+
+      <div className="container">
+        <p className="title">Signup</p>
+        <form onSubmit={handleSubmit}>
+          <input type="email"  placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          <input type="submit" value="Signup" className="btn"/>
+        </form>
+        <p>Already have a account?? <Link to="/login" style={{textDecoration:"none",color:"inherit"}}>
+          Login
+        </Link> </p>
+        
+      </div>
+        
       
     </div>
   )
