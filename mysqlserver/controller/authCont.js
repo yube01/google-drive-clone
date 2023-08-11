304cb681feebba8cdddb0a20ec54947e0b8c2d67
yube01 <yubraja008@gmail.com>
2023-08-11 20:31:02 +0545

    basic file on route and controller

---
+++ mysqlserver/controller/authCont.js
@@ -0,0 +1,8 @@
+export const  register = async(req,res)=>{
+
+}
+
+
+export const login  = async(req,res)=>{
+
+}
---
+++ mysqlserver/controller/fileCont.js
@@ -0,0 +1,7 @@
+export const createFile = async(req,res)=>{
+
+}
+
+export const deleteFile = async(req,res)=>{
+    
+}
